8610e34be424524a453183edf627bf3e781fb757
Rakesh60 <rakeshgupta5353@gmail.com>
2024-01-22 23:32:38 +0530

    link mongo dg

--- database/db.js
+++ database/db.js
@@ -1,16 +1,14 @@
 //rakesh12345
 //mongodb+srv://rakeshibm909:rakesh12345@meardatabase.fzh6dii.mongodb.net/eNotebook
-import {connect} from 'mongoose'
+import { connect } from "mongoose";
 
-const connectToMongo=async()=>{
- 
-    try {
-        await connect(process.env.MONGO_URI);
-        console.log("----Database connected Successfully")
-        
-    } catch (error) {
-        
-    }
-
-}
-export default connectToMongo
+const connectToMongo = async () => {
+  try {
+    //await connect(process.env.MONGO_URI);
+    await connect(
+      "mongodb+srv://rakeshibm909:rakesh12345@meardatabase.fzh6dii.mongodb.net/eNotebook"
+    );
+    console.log("----Database connected Successfully");
+  } catch (error) {}
+};
+export default connectToMongo;
