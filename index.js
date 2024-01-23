6fb2e8d40311b3142f2204544f2bd25365e9115c
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-23 10:20:34 +0530

    ok04

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ const port = 4000;
 // middle ware
 app.use(express.json());
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: [
-//       "https://vercel.com/rakesh-kumar-guptas-projects/notebook-backend",
-//     ],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
 
 // Available routes
 app.get("/", (req, res) => {
